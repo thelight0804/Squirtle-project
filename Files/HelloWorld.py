ef328b8e13f6742ba9223a3a6b3e408d0cf48b8e
thelight0804 <thelight0804@gmail.com>
2022-02-12 10:55:44 +0900

    add Files & modify README.md
    HelloWorld.py 추가
    .gitignore 추가
    README.md에서 설명 제거

---
+++ Files/HelloWorld.py
@@ -0,0 +1,23 @@
+# cmd에서 pip3 install PyQt5 명령어를 이용하여 PyQt5를 설치해야 한다
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('My First Application')
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
