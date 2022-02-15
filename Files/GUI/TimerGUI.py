2bda9fc1ab71a54fd372865638e230de71ca3d9c
thelight0804 <thelight0804@gmail.com>
2022-02-15 17:12:49 +0900

    TimerGUI add Button
    - TimerGUI.py
        - 시와 분 사이에 ‘ : ’ 추가
        - HCombo와 MCombo에 요소 추가
        - 초기화(Reset), 타이머 변경 (ChangeBtn), 시작 (Start) 버튼 추가
        - 진행 바 추가

--- Files/GUI/TimerGUI.py
+++ Files/GUI/TimerGUI.py
@@ -5,10 +5,11 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtGui import QFont
+from PyQt5.QtCore import *
+
 
 
 class MyApp(QWidget):
-
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -20,17 +21,51 @@ class MyApp(QWidget):
         self.resize(1080, 720) #창 사이즈
 
         ##버튼 구현
+        #환경설정 버튼
         ConfigBtn = QPushButton('', self) #ConfigBtn 버튼 구현
         ConfigBtn.setIcon(QtGui.QIcon('..\..\Assets\icon\config.svg')) #아이콘 구현 (상대경로)
         ConfigBtn.setIconSize(QtCore.QSize(50,50)) #아이콘 크기
         ConfigBtn.setFlat(True) #버튼 테두리 없애기
         ConfigBtn.clicked.connect(self.ConfigBtnClicked) #버튼 클릭 했을 때 ConfigBtn_clicked 함수 호출
 
+        #Reset 버튼
+        ResetBtn = QPushButton('', self)
+        ResetBtn.setIcon(QtGui.QIcon('..\..\Assets\icon\\reset.svg')) #\r은 옵션이라 \\r를 사용하였다
+        ResetBtn.setIconSize(QtCore.QSize(50,50))
+        ResetBtn.setFlat(True)
+
+        #타이머 변경 버튼
+        ChangeBtn = QPushButton('사용자\n타이머', self)
+        ChangeBtn.setIconSize(QtCore.QSize(50,50))
+
+        #Start 버튼
+        self.StartBtn = QPushButton('', self)
+        self.StartBtn.setIcon(QtGui.QIcon('..\..\Assets\icon\start.svg'))
+        self.StartBtn.setIconSize(QtCore.QSize(50,50))
+        self.StartBtn.setFlat(True)
+
         ##ComboBox 구현
         HCombo = QComboBox(self) #시
         MCombo = QComboBox(self) #분
-        HCombo.addItem('1')
-        MCombo.addItem('1')
+        for i in range(1, 13): #Hour 시간 추가
+            if i<10: #1의 자리 '0' 추가
+                HCombo.addItem('0'+str(i))
+            else:
+                HCombo.addItem(str(i))
+        for i in range(0, 12): #Min 시간 추가
+            if i<2:
+                MCombo.addItem('0'+str(i*5))
+            else:
+                MCombo.addItem(str(i*5))
+
+        ##QLable 구현
+        label = QLabel(':', self)
+        label.setAlignment(Qt.AlignCenter)
+
+        ##상태바 구현
+        self.TimerBar = QProgressBar() #TODO: 원형 바 어떻게 해야 할까..ㅠ
+        self.timer = QTimer()
+        self.step = 0
 
         ##Box 레이아웃
         #수평
@@ -39,18 +74,29 @@ class MyApp(QWidget):
         hboxUp.addStretch(1) #비율이 1인 빈 공간 생성
         
         hboxMid = QHBoxLayout() #ComboBox 레이아웃
-        hboxMid.setContentsMargins(450, 0, 450, 0)
-        hboxUp.addStretch(1)
+        hboxMid.setContentsMargins(450, 0, 450, 0) #Left, Up, Right, Down
         hboxMid.addWidget(HCombo)
+        hboxMid.addWidget(label)
         hboxMid.addWidget(MCombo)
-        hboxUp.addStretch(1)
+        hboxMid.addWidget(self.TimerBar)
+
+        hboxDown = QHBoxLayout() #초기와, 타이머변경, 시작 레이아웃
+        hboxDown.addStretch(10)
+        hboxDown.addWidget(ResetBtn)
+        hboxDown.addStretch(1)
+        hboxDown.addWidget(ChangeBtn)
+        hboxDown.addStretch(1)
+        hboxDown.addWidget(self.StartBtn)
+        hboxDown.addStretch(10)
         
         #수직
         vbox = QVBoxLayout() #수직 box 생성
         vbox.addLayout(hboxUp)
-        vbox.addStretch(1)
+        vbox.addStretch(10)
         vbox.addLayout(hboxMid)
-        vbox.addStretch(1)
+        vbox.addStretch(10)        
+        vbox.addLayout(hboxDown)
+        vbox.setContentsMargins(0, 0, 0, 50)
 
         self.setLayout(vbox) #수직 box를 메인 레이아웃으로 설정
 
@@ -61,11 +107,16 @@ class MyApp(QWidget):
     def ConfigBtnClicked(self): #Config 버튼 클릭
         QMessageBox.about(self, 'MessageBox', '클릭!') #QMessageBox 메세지 박스
 
-    #def hComboSet(hbox):
 
+    def timerEvent(self, e): #타이머 이벤트
+        if self.step >= 100:
+            self.timer.stop()
+            self.TimerBar.setValue(0)
+            return
 
-    
-    
+        self.step = self.step + 1
+        self.TimerBar.setValue(self.step)
+        
     def center(self): #창의 화면을 중앙으로
         qr = self.frameGeometry() #창의 위치와 크기 정보를 가져온다
         cp = QDesktopWidget().availableGeometry().center() #현재 모니터의 위치를 파악한다
