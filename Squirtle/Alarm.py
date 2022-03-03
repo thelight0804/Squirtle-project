a9b64ad47510008e3d7436f25cdf38c183c8ee1d
thelight0804 <thelight0804@gmail.com>
2022-03-03 17:34:23 +0900

    Print Hour 0 Fix
    - 버그 수정
        - Hour를 설정해도 0으로 출력되는 버그 수정 (Timer.py - TimeUpdate())
    - **Alarm.py**
        - 그냥 버튼 없이 win10toast 라이브러리 사용
        - Winotify를 사용하려고 했으나 버튼을 통한 함수 호출이 없음

--- Squirtle/Alarm.py
+++ Squirtle/Alarm.py
@@ -10,8 +10,9 @@ class alarm:
         self.__AgainMin = AgainMin
     
     def call(self): #Windows 10 알람
-        toaster = ToastNotifier() #win10toast의 ToastNotifier 객체 생성
-        toaster.show_toast(title="1시간이 지났습니다", msg="스트레칭을 해주세요", icon_path="..\Assets\icon\Squirtle.ico", duration=10)
+        toast = ToastNotifier() #win10toast의 ToastNotifier 객체 생성
+        toast.show_toast(title="1시간이 지났습니다", msg="스트레칭을 해주세요", icon_path="..\Assets\icon\Squirtle.ico", duration=10)
+        #TODO toast 알림창을 이용한 상호교환도 하고 싶은데..
 
     #property & setter
     @property
