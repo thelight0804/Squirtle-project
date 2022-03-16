04511c2fee5bb862fc36f750ec298bec9859ea8c
thelight0804 <thelight0804@gmail.com>
2022-03-16 15:59:55 +0900

    add ConfigGUI
    - ConfigGUI.ui, ConfigGUI.py
        - 환경설정 GUI 구현
        - 환경설정 클릭 시 다음 Window 출력 구현

--- Squirtle/GUI/ConfigGUI.py
+++ Squirtle/GUI/ConfigGUI.py
@@ -1,17 +1,27 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5 import uic
 
-class ConfigGUI(QDialog):
-    def __init__(self):
+#UI파일 연결
+#단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
+form_class = uic.loadUiType("GUI\ConfigGUI.ui")[0]
+
+#화면을 띄우는데 사용되는 Class 선언
+class ConfigGUI(QDialog, form_class) :
+    def __init__(self) :
         super().__init__()
-        self.initUI()
+        self.setupUi(self)
+        #self.show()
 
-    def initUI(self):
-        self.setWindowTitle('Config')
-        self.setGeometry(100, 100, 200, 100)
-        self.show()
-#TODO Dialog가 자동으로 꺼지는 거 수정
-if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
+# if  __name__ == "__main__" :
+#     #QApplication : 프로그램을 실행시켜주는 클래스
+#     app = QApplication(sys.argv) 
+
+#     #ConfigGUI의 인스턴스 생성
+#     myWindow = ConfigGUI() 
+
+#     #프로그램 화면을 보여주는 코드
+#     myWindow.show()
+
+#     #프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
+#     app.exec_()
