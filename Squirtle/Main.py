8c234f60799e1b1a8beb87b36ec41af7049bf05e
thelight0804 <thelight0804@gmail.com>
2022-07-31 22:18:06 +0900

    design: Fix Japanese translation that Alert Setting
    - '알람'표기의 「アラ-ム」를「通知」로 변경하였습니다.
    - 소스코드의 기본 정보를 Python Documentation에 맞추어서 수정하였습니다.

--- Squirtle/Main.py
+++ Squirtle/Main.py
@@ -1,10 +1,12 @@
 """
-Date : 22.02.01 ~ 22.05.09
-Last Update : 22.07.18
-Title : Squirtle (꼬부기)
-
-@thelight0804
-@ING'S (Nifskor)
+Date:
+    22.02.01 ~ 22.05.09
+Last Update:
+    22.07.31
+author:
+    @thelight0804
+Thanks to:
+    @ING'S (Nifskor)
 """
 #프로그램 실행 시 가장 처음 실행되는 코드
 import Timer, FileModule
